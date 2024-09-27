b43f6b54aa53cae8b09ae60ef86b5f3ee07c5687
Terry Deng <terry.yimin.deng@gmail.com>
2024-09-27 16:38:11 -0400

    Fix security vulnerability.

--- src/main/java/com/clariti/homework/fee/ClaritiHomeworkController.java
+++ src/main/java/com/clariti/homework/fee/ClaritiHomeworkController.java
@@ -3,7 +3,7 @@ package com.clariti.homework.fee;
 import com.clariti.homework.fee.model.FeeRequest;
 import com.clariti.homework.fee.model.TotalFee;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequiredArgsConstructor
 public class ClaritiHomeworkController {
 
     private final ClaritiHomeworkService claritiHomeworkService;
-
-    @Autowired
-    public ClaritiHomeworkController(ClaritiHomeworkService claritiHomeworkService) {
-        this.claritiHomeworkService = claritiHomeworkService;
-    }
 
     @ApiOperation("Calculate the total fee given a combination of department, category, sub category and type.")
     @PostMapping("/fees")
